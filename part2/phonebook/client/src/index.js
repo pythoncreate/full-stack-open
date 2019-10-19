2bb7062374fa56bdc0911e5d9f60dc664e8e9ed9
Chris Stuart <crstuart@gmail.com>
2019-10-19 18:53:03 -0400

    'new;

--- part2/phonebook/client/src/index.js
+++ part2/phonebook/client/src/index.js
@@ -1,17 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
-
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
 
+import axios from "axios";
+
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-    { name: "Dan Abramov", number: "12-43-234345" },
-    { name: "Mary Poppendieck", number: "39-23-6423122" }
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filter, setFiltered] = useState("");
@@ -29,6 +25,14 @@ const App = () => {
         {p.name} {p.number}
       </p>
     ));
+
+  useEffect(() => {
+    console.log("effect");
+    axios.get("http://localhost:3001/persons").then(response => {
+      console.log("promise fulfilled");
+      setPersons(response.data);
+    });
+  }, []);
 
   const addNameNum = event => {
     event.preventDefault();
