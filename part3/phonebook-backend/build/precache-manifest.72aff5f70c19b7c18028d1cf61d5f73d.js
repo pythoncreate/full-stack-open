32da7a76d368b46ba17497365e4a841010e45ad7
Chris Stuart <crstuart@gmail.com>
2019-10-24 19:14:41 -0400

    fixed folders

---
+++ part3/phonebook-backend/build/precache-manifest.72aff5f70c19b7c18028d1cf61d5f73d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97ce71e4924a17614d32e2475a420bf6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4d68b7c6fd8e3b49503",
+    "url": "/static/css/main.b67c54cc.chunk.css"
+  },
+  {
+    "revision": "d84c00cabc75bb72fe88",
+    "url": "/static/js/2.0ccef00c.chunk.js"
+  },
+  {
+    "revision": "e4d68b7c6fd8e3b49503",
+    "url": "/static/js/main.b772ddcf.chunk.js"
+  },
+  {
+    "revision": "875d570ce1b6fc43afc6",
+    "url": "/static/js/runtime-main.e077b804.js"
+  }
+]);
---
+++ part3/phonebook-backend/models/person.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+const username = process.env.MONGODB_USER;
+const password = process.env.MONGODB_PASS;
+
+const url = `mongodb+srv://${username}:${password}@cluster0-ydrdq.mongodb.net/numbers?retryWrites=true&w=majority`;
+
+mongoose
+  .connect(url, {
+    useNewUrlParser: true
+  })
+  .then(result => {
+    console.log("connected to MongoDB");
+  })
+  .catch(error => {
+    console.log("Error while connecting to MongoDB:", error.message);
+    process.exit(1);
+  });
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String
+});
+
+personSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  }
+});
+
+module.exports = mongoose.model("Person", personSchema);
